45d8f08a7226c9b9deafee670159bac31a39e160
abhishekdelta <i.abhi27@gmail.com>
2012-09-22 13:28:04 +0530

    Mods

--- ToggleImages/background.js
+++ ToggleImages/background.js
@@ -1,12 +1,14 @@
 var state = 0;
-chrome.pageAction.show();
 
-chrome.pageAction.onClicked.addListener(function(tab) {
+chrome.browserAction.setBadgeText({text: ' '});
+chrome.browserAction.setBadgeBackgroundColor({color: '#CDCDCD'});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
 
     var msg = {action: 'toggle'};
     chrome.tabs.sendRequest(tab.id, msg, function(response) {            
           if(response.result == 1) {
-           // chrome.browserAction.setBadgeBackgroundColor({color: (state == 0 ? '#FF0000' : '#CDCDCD')});
+            chrome.browserAction.setBadgeBackgroundColor({color: (state == 0 ? '#FF0000' : '#CDCDCD')});
             state = (state == 0 ? 1 : 0);            
           }          
     });
